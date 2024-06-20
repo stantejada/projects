45e0cabf4e4c5e8e842405235782a4b283e89efb
Diego Tejada <stanleytejadaj@gmail.com>
2024-06-19 20:04:47 -0700

    Making the models for database

---
+++ blogAPI/db/models.py
@@ -0,0 +1,24 @@
+from db.database import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    email = Column(String, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    
+    blogs = relationship("Blog", back_populates="owner")
+    
+class Blog(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(String, index=True)
+    pub_date = Column(DateTime, server_default=func.now())
+    user_id = Column(Integer, ForeignKey("users.id"))
+    
+    owner = relationship("User", back_populates="blogs")
